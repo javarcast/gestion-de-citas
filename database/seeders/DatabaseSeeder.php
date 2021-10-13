5f40f39e1bbc6193b169a0b915420be5e63c1a5d
Jesus Varela <javarcast93@gmail.com>
2021-10-13 18:09:48 -0400

    configuracion de los faker, seeder y migraciones

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Treatment;
+use App\Models\Patient;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call([RolSeeder::class]);
+        User::factory()->create([
+            'email' => 'admin@smileclinic.com',
+            'password' => bcrypt('123456'),
+            'name' => 'Smile Clinic',
+            'phone_number' => '123456789',
+            'rol_id' => 1,
+            'dni'=> '1',
+        ]);
+
+        User::factory(10)->create();
+
+        Patient::factory(50)->create();
+        $this->call([TreatmentSeeder::class]);
     }
 }
