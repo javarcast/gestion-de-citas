977074ac507f051393925a658c1c28187d58d0a5
Jesus Varela <javarcast93@gmail.com>
2021-10-29 20:50:06 -0400

    ajustando colores

--- resources/js/Components/InputCheck.vue
+++ resources/js/Components/InputCheck.vue
@@ -1,7 +1,7 @@
 <template>
         <label  class="inline-flex flex justify-between items-center w-full p-3 hover:bg-gray-50 cursor-pointer">
             {{treatment.name}}
-            <input @click="sendEmitClick" class="text-red-500 w-6 h-6 mr-2 focus:ring-red-400 focus:ring-opacity-25 border border-gray-300 rounded" type="checkbox" />
+            <input @click="sendEmitClick" class="text-blue-900 w-6 h-6 mr-2 focus:ring-grenn-300 focus:ring-opacity-25 border border-gray-300 rounded" type="checkbox" />
         </label>
 </template>
 
