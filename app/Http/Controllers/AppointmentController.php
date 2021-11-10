8fe9bfc76d58f64899660dfee2d79d63afc92c6d
wladimirGO <wladimir.gonzalez@unet.edu.ve>
2021-11-09 21:49:23 -0400

    ajuste en update citas

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -41,7 +41,7 @@ class AppointmentController extends Controller
                         ['appointments.date', 'LIKE', "%$myArray[1]%"],
                     ])
                     ->select('appointments.id','date','hour','users.name as doctor','patients.name as paciente')
-                    ->paginate(27);
+                    ->paginate(7);
                 }
                 else{
                     //Es Admin
@@ -64,7 +64,7 @@ class AppointmentController extends Controller
                         ['appointments.date', 'LIKE', "%$myArray[1]%"],
                     ])
                     ->select('appointments.id','date','hour','users.name as doctor','patients.name as paciente')
-                    ->paginate(27);
+                    ->paginate(7);
                 }
                 
             }
@@ -254,41 +254,41 @@ class AppointmentController extends Controller
         ->select('user_id')
         ->get();
         if(count($cita)>0){
-            return "El doctor ya tiene una cita asignada en ese horario";
+            $message="El doctor ya tiene una cita asignada en ese horario";
+            return redirect()->back()->with('status',$message);
         }
         else{
             $appointment = Appointment::findOrFail($id);
-        $appointment->date=$request->get('date');
-        $appointment->hour=$request->get('time');
-        $appointment->status_id=$request->get('status_id');
-        $appointment->patient_id=$request->get('patient_id');
-        $appointment->user_id=$request->get('doctor_id');
-        $appointment->total=$request->get('total');
-        $appointment->update();
+            $appointment->date=$request->get('date');
+            $appointment->hour=$request->get('time');
+            $appointment->status_id=$request->get('status_id');
+            $appointment->patient_id=$request->get('patient_id');
+            $appointment->user_id=$request->get('doctor_id');
+            $appointment->total=$request->get('total');
+            $appointment->update();
 
-        $aux=$request->get('list');
+            $aux=$request->get('list');
 
-        $appointmentTrea=AppoimentTreatments::
-        where("appointment_id","=",$id)
-        ->get();
+            $appointmentTrea=AppoimentTreatments::
+            where("appointment_id","=",$id)
+            ->get();
 
-        $appointmentTrea->each->delete();
+            $appointmentTrea->each->delete();
 
-        $cont=0;
-        while ($cont < count($aux)) {
-            if($aux[$cont]['count']>0){
-                $appointmentTrea= new AppoimentTreatments();
-                $appointmentTrea->appointment_id=$appointment->id;
-                $appointmentTrea->treatment_id=$aux[$cont]['treatment_id'];
-                $appointmentTrea->amount=$aux[$cont]['amount'];
-                $appointmentTrea->count=$aux[$cont]['count'];
-                $appointmentTrea->save();
+            $cont=0;
+            while ($cont < count($aux)) {
+                if($aux[$cont]['count']>0){
+                    $appointmentTrea= new AppoimentTreatments();
+                    $appointmentTrea->appointment_id=$appointment->id;
+                    $appointmentTrea->treatment_id=$aux[$cont]['treatment_id'];
+                    $appointmentTrea->amount=$aux[$cont]['amount'];
+                    $appointmentTrea->count=$aux[$cont]['count'];
+                    $appointmentTrea->save();
+                }
+                $cont++;
             }
-            $cont++;
-        }
-        $message = "La Cita ha sido Actualizada!!"; 
-        //return redirect()->route('usuarios.index');
-        return redirect()->route('citas.index')->with('status',$message);
+            $message = "La Cita ha sido Actualizada!!"; 
+            return redirect()->route('citas.index')->with('status',$message);
         }
         
     }
